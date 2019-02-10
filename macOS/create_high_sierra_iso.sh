70b43dd8743860189e7d64b11ee957c1c80f9d96
Andy Wright <atwright147@gmail.com>
2019-02-10 15:45:20 +0000

    add script to create macOS High Sierra ISO

---
+++ macOS/create_high_sierra_iso.sh
@@ -0,0 +1,8 @@
+hdiutil create -o /tmp/HighSierra.cdr -size 7000m -layout SPUD -fs HFS+J &&\
+hdiutil attach /tmp/HighSierra.cdr.dmg -noverify -mountpoint /Volumes/install_build &&\
+sudo /Applications/Install\ macOS\ High\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/install_build &&\
+mv /tmp/HighSierra.cdr.dmg ~/Desktop/InstallSystem.dmg &&\
+hdiutil detach /Volumes/Install\ macOS\ High\ Sierra &&\
+hdiutil convert ~/Desktop/InstallSystem.dmg -format UDTO -o ~/Desktop/HighSierra.iso &&\
+mv ~/Desktop/HighSierra.iso.cdr ~/Desktop/HighSierra.iso &&\
+rm ~/Desktop/InstallSystem.dmg
